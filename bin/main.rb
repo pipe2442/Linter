18fffa9775b86027b28c15fc21eaa62928b28d3c
pipe2442 <felipeangulo2442@gmail.com>
2021-01-19 19:36:53 -0500

    Added parenthesis counter feature

---
+++ bin/main.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+require 'readline'
+require_relative '../lib/linters.rb'
+
+linter = Linter.new('../lib/example.rb')
+linter.find_parenthesis
+
+#
+# file = File.open('../lib/example.rb', "r")
+# file.each do |i|
+#     puts  i
+# end
+#
+
+# file = File.open('../lib/example.rb', "r")
+# file.readlines().each do |i|
+#     puts i
+# end
+
+# #file = "how are you hello world"
+# #p file =~ /hello/ ? "Valid" : "Invalid"
+
+#
+# file = IO.readlines('../lib/example.rb')
+#
+# puts  file[2]
+#
+#
+# file.each do |i|
+#    if i == 'como'
+#         puts 'verdadero'
+#     else
+#         puts 'falso'
+#     end
+# end
